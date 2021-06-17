e06d86e0964a8b03d2e039269e463c58d0fe5d0a
olseason <sjlee2177@naver.com>
2021-06-17 19:20:51 +0900

    06/17_수업

---
+++ src/echo/ex01/Server.java
@@ -0,0 +1,31 @@
+package echo.ex01;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException{
+		
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("172.30.1.25",10001));
+		
+		
+		
+		System.out.println("<서버시작>");
+		System.out.println("======================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		Socket socket = serverSocket.accept();
+		System.out.println("[클라이언트가 연결되었습니다.]");
+		
+		
+		socket.close();
+		
+		serverSocket.close();
+
+	}
+
+}
